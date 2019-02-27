b5fd90b6e533c4065adb2c6e6b7adb54d5433356
Jake Lee <louisoel@gmail.com>
2019-02-26 23:28:10 -0500

    Shortcut button to Heat Seek app

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,11 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function() {
+function loginTab() {
+  chrome.tabs.create({
+    url: 'https://app.heatseek.org/users/sign_in'
+  });
+}
 
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('heatseek-app').addEventListener('click', loginTab);
 });
